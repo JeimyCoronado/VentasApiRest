62197ec4928e9e23f68b3050ae1ec7fc76bd6db5
JeimyCoronado <jeimycoronado100@gmail.com>
2022-08-08 21:49:27 -0500

    ComponentScan

--- src/main/java/com/idat/springbootapirestventas/app/SpringBootApirestVentasApplication.java
+++ src/main/java/com/idat/springbootapirestventas/app/SpringBootApirestVentasApplication.java
@@ -2,8 +2,10 @@ package com.idat.springbootapirestventas.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.idat.springbootapirestventas.app.controller"})
 public class SpringBootApirestVentasApplication {
 
 	public static void main(String[] args) {

ed134d5ce217adb9b1836e3b9a6fed075dc59143
JeimyCoronado <jeimycoronado100@gmail.com>
2022-08-08 22:00:19 -0500

    Correxion componentscan

--- src/main/java/com/idat/springbootapirestventas/app/SpringBootApirestVentasApplication.java
+++ src/main/java/com/idat/springbootapirestventas/app/SpringBootApirestVentasApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.idat.springbootapirestventas.app.controller"})
+@ComponentScan({"com.idat.springbootapirestventas.app/controller"})
 public class SpringBootApirestVentasApplication {
 
 	public static void main(String[] args) {
